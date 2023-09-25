24d15329564761a1f585d8fd536927ffa4190d17
Simon Polstra <s.polstra@uva.nl>
2023-09-25 14:11:12 +0200

    simple roi marker where wrong for global in_roi

--- examples/simple_add_openmp.c
+++ examples/simple_add_openmp.c
@@ -6,16 +6,15 @@
 int main(void) {
     int sum = 0;
 
-    // race condition!
-    // atomic_trace::start_roi(); // This way we include a lot of openmp overhead
+    atomic_trace::start_roi(); // This way we include a lot of openmp overhead
     #pragma omp parallel
     {
         // printf("omp_get_num_threads(): %d\n", omp_get_num_threads());
-        atomic_trace::start_roi();
+        // atomic_trace::start_roi(); // needs in roi_per_thread branch
         sum += 1;
-        atomic_trace::end_roi();
+        // atomic_trace::end_roi();
     }
-    // atomic_trace::end_roi(); // This way we include a lot of openmp overhead
+    atomic_trace::end_roi(); // This way we include a lot of openmp overhead
 
     printf("sum: %d\n", sum);
     return 0;
