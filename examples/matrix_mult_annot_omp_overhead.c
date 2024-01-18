49cf12c4b2aa0edbcad1471a9ea0b1cbbc42bfd1
Simon Polstra <s.polstra@uva.nl>
2024-01-18 15:27:12 +0100

    Added matrix mult variants.
    Different omp constructs, with and without omp overhead tracing.

---
+++ examples/matrix_mult_annot_omp_overhead.c
@@ -0,0 +1,62 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <sys/time.h>
+
+#include "src/annotation.h"
+
+#define N 10
+
+// for i in 1 2 4 8 12 16 32; do echo -n "$i threads: "; OMP_NUM_THREADS=$i ./matrix_mult; done
+
+int A[N][N];
+int B[N][N];
+int C[N][N];
+
+// Only trace computation, all OMP stuff excluded.
+
+int main() 
+{
+    int i,j,k;
+    struct timeval tv1, tv2;
+    struct timezone tz;
+	double elapsed; 
+    // omp_set_num_threads(omp_get_num_procs());
+    // printf("Running on %d threads\n", omp_get_num_threads());
+
+    for (i= 0; i< N; i++)
+        for (j= 0; j< N; j++)
+	{
+            A[i][j] = 2;
+            B[i][j] = 2;
+	}
+    gettimeofday(&tv1, &tz);
+    #pragma omp parallel private(i,j,k) shared(A,B,C)
+    {
+        start_roi();
+        #pragma omp for
+        for (i = 0; i < N; ++i) {
+            for (j = 0; j < N; ++j) {
+                for (k = 0; k < N; ++k) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+        end_roi();
+    }
+
+
+    gettimeofday(&tv2, &tz);
+    elapsed = (double) (tv2.tv_sec-tv1.tv_sec) + (double) (tv2.tv_usec-tv1.tv_usec) * 1.e-6;
+    printf("elapsed time = %f seconds.\n", elapsed);
+
+    for (i= 0; i< N; i++)
+    {
+        for (j= 0; j< N; j++)
+        {
+            printf("%d\t",C[i][j]);
+        }
+        printf("\n");
+    }
+}
